f600fce97ba09a7ba7001a3460cd2dfefbba13c4
UroKbro <kumbhataadi@gmail.com>
2026-06-07 22:00:44 -0700

    Initial Commit

---
+++ VoxelForge/VoxelForgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoxelForgeApp.swift
+//  VoxelForge
+//
+//  Created by Aadi Kumbhat on 6/7/26.
+//
+
+import SwiftUI
+
+@main
+struct VoxelForgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
